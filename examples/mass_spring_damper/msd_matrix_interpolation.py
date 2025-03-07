c241a2c0980c984b34895f273fb1d66e03e60c3b
jrettberg <johannes.rettberg@itm.uni-stuttgart.de>
2025-03-07 15:07:45 +0100

    new matrix_interpolation framework

--- examples/mass_spring_damper/msd_matrix_interpolation.py
+++ examples/mass_spring_damper/msd_matrix_interpolation.py
@@ -3,6 +3,8 @@ import logging
 import os
 import numpy as np
 import copy
+from scipy.spatial import ConvexHull
+
 
 # third party packages
 import tensorflow as tf
@@ -14,8 +16,8 @@ import matplotlib.pyplot as plt
 # own packages
 import aphin.utils.visualizations as aphin_vis
 from aphin.identification import PHIN
-from aphin.layers import PHLayer
-from aphin.utils.data import Dataset, PHIdentifiedDataset
+from aphin.layers import PHLayer, PHQLayer
+from aphin.utils.data import Dataset, PHIdentifiedDataset, PHIdentifiedData
 from aphin.utils.callbacks_tensorflow import callbacks
 from aphin.utils.configuration import Configuration
 from aphin.utils.save_results import (
@@ -24,12 +26,32 @@ from aphin.utils.save_results import (
     save_evaluation_times,
     save_training_times,
 )
+from aphin.utils.transformations import (
+    reshape_states_to_features,
+    reshape_features_to_states,
+)
 from aphin.utils.print_matrices import print_matrices
+from state_space_ph.matrix_interpolation import (
+    get_coeff_values,
+    evaluate_interpolation,
+    get_weighting_function_values,
+    evaluate_matrices,
+    in_hull,
+)
+from aphin.systems import LTISystem
+
+# tf.config.run_functions_eagerly(True)
 
 
-def phin_learning(msd_data, msd_cfg, config_dirs):
+def phin_learning(msd_data, msd_cfg, config_dirs, dir_extension: str = ""):
 
     data_dir, log_dir, weight_dir, result_dir = config_dirs
+    log_dir = os.path.join(log_dir, dir_extension)
+    weight_dir = os.path.join(weight_dir, dir_extension)
+    result_dir = os.path.join(result_dir, dir_extension)
+    for dir in [log_dir, weight_dir, result_dir]:
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
 
     t, x, dx_dt, u, mu = msd_data.data
     n_sim, n_t, n_n, n_dn, n_u, n_mu = msd_data.shape
@@ -56,12 +78,12 @@ def phin_learning(msd_data, msd_cfg, config_dirs):
     )
 
     regularizer = tf.keras.regularizers.L1L2(l1=msd_cfg["l1"], l2=msd_cfg["l2"])
-    system_layer = PHLayer(
+    system_layer = PHQLayer(
         n_f,
         n_u=n_u,
         n_mu=n_mu,
         regularizer=regularizer,
-        name="ph_layer",
+        name="phq_layer",
         layer_sizes=msd_cfg["layer_sizes_ph"],
         activation=msd_cfg["activation_ph"],
     )
@@ -73,7 +95,11 @@ def phin_learning(msd_data, msd_cfg, config_dirs):
         optimizer=tf.keras.optimizers.Adam(learning_rate=msd_cfg["lr"]),
         loss=tf.keras.losses.MSE,
     )
-    phin.build(input_shape=([x.shape, dx_dt.shape, u.shape, mu.shape], None))
+    if mu is None:
+        input_shape = [x.shape, dx_dt.shape, u.shape]
+    else:
+        input_shape = [x.shape, dx_dt.shape, u.shape, mu.shape]
+    phin.build(input_shape=(input_shape, None))
 
     # phin.load_weights(data_path_weights_filename)
     if msd_cfg["load_network"]:
@@ -83,7 +109,10 @@ def phin_learning(msd_data, msd_cfg, config_dirs):
         # n_train = int(0.8 * x.shape[0])
         # x_train = [x[:n_train], dx_dt[:n_train], u[:n_train], mu[:n_train]]
         # x_val = [x[n_train:], dx_dt[n_train:], u[n_train:], mu[n_train:]]
-        x_train = [x, dx_dt, u, mu]
+        if mu is None:
+            x_train = [x, dx_dt, u]
+        else:
+            x_train = [x, dx_dt, u, mu]
         logging.info(f"Fitting NN weights.")
         train_hist = phin.fit(
             x=x_train,
@@ -169,8 +198,27 @@ def main(config_path_to_file=None):
             f"File could not be found. If this is the first time you run this example, please execute the data generating script `./state_space_ph/mass_spring_damper_data_gen.py` first."
         )
 
-    train_idx = np.arange(30)
-    test_idx = np.arange(30, 36)
+    _, idx = np.unique(msd_data.Mu, axis=0, return_index=True)
+    mu_all = msd_data.Mu[np.sort(idx), :]
+    hull_convex = ConvexHull(mu_all)
+    idx_train = hull_convex.vertices
+    idx_test = np.setdiff1d(np.arange(mu_all.shape[0]), idx_train)
+    assert idx_test.shape[0] >= 3  # at least 3 test trajectories
+
+    train_idx = np.array(
+        [
+            (i_same_mu_scenario) * 3 + np.array([0, 1, 2])
+            for i_same_mu_scenario in idx_train
+        ]
+    ).flatten()
+    test_idx = np.array(
+        [
+            (i_same_mu_scenario) * 3 + np.array([0, 1, 2])
+            for i_same_mu_scenario in idx_test
+        ]
+    ).flatten()
+    # train_idx = np.arange(30)
+    # test_idx = np.arange(30, 36)
 
     # split into train and test data
     msd_data.train_test_split_sim_idx(sim_idx_train=train_idx, sim_idx_test=test_idx)
@@ -178,39 +226,223 @@ def main(config_path_to_file=None):
     msd_data.scale_Mu(desired_bounds=msd_cfg["desired_bounds"])
     msd_data.states_to_features()
 
-    num_train_scenarios_with_same_mu = 10
-    msd_data_orig = copy.deepcopy(msd_data)
-    scenario_list = []
-    for i_same_mu_scenario in range(num_train_scenarios_with_same_mu):
+    # usual PHIN framework with mu DNN
+    msd_data_id, system_layer_interp, phin_interp = phin_learning(
+        msd_data, msd_cfg, configuration.directories, dir_extension="usual_phin "
+    )
 
-        msd_data = copy.deepcopy(msd_data_orig)
-        # there are always 3 scenarios with the same mu but different inputs
-        sim_idx = (i_same_mu_scenario) * 3 + np.array([0, 1, 2])
-        msd_data.decrease_num_simulations(sim_idx=sim_idx)
-        msd_data_id, system_layer, phin = phin_learning(
-            msd_data, msd_cfg, configuration.directories
+    result_dir_usual_phin = f"{result_dir}_usual_phin"
+    if not os.path.isdir(result_dir_usual_phin):
+        os.mkdir(result_dir_usual_phin)
+    use_train_data = False
+    idx_gen = "rand"
+    msd_data.calculate_errors(msd_data_id)
+    aphin_vis.plot_time_trajectories_all(
+        msd_data, msd_data_id, use_train_data, idx_gen, result_dir_usual_phin
+    )
+
+    load_matrices = False
+    if load_matrices:
+        matrices = np.load(os.path.join(result_dir, "JRBQ_matrix_interpolation.npz"))
+        J_all = matrices["J"]
+        R_all = matrices["R"]
+        Q_all = matrices["Q"]
+        B_all = matrices["B"]
+        A_all = matrices["A"]
+        parameter_array = matrices["parameter_array"]
+
+    else:
+        # fit matrices
+        num_train_scenarios_with_same_mu = 10
+        msd_data_orig = copy.deepcopy(msd_data)
+        scenario_list = []
+        for i_same_mu_scenario in range(num_train_scenarios_with_same_mu):
+
+            msd_data = copy.deepcopy(msd_data_orig)
+            # there are always 3 scenarios with the same mu but different inputs
+            sim_idx = (i_same_mu_scenario) * 3 + np.array([0, 1, 2])
+            msd_data.decrease_num_simulations(sim_idx=sim_idx)
+            mu_orig = msd_data.TRAIN.Mu[0, :]
+            msd_data.remove_mu()
+            msd_data_id_interp_scenario, system_layer_interp, phin_interp = (
+                phin_learning(
+                    msd_data,
+                    msd_cfg,
+                    configuration.directories,
+                    dir_extension=f"interp{i_same_mu_scenario}",
+                )
+            )
+            scenario_list.append(
+                (
+                    msd_data_id_interp_scenario,
+                    system_layer_interp,
+                    phin_interp,
+                    msd_data,
+                    mu_orig,
+                )
+            )
+
+        list_matrices = []
+        parameter_array = np.zeros((len(scenario_list), msd_data_orig.TRAIN.n_mu))
+        A_all = np.zeros((msd_data.TRAIN.n_f, msd_data.TRAIN.n_f, len(scenario_list)))
+        Q_all = np.zeros((msd_data.TRAIN.n_f, msd_data.TRAIN.n_f, len(scenario_list)))
+        J_all = np.zeros((msd_data.TRAIN.n_f, msd_data.TRAIN.n_f, len(scenario_list)))
+        R_all = np.zeros((msd_data.TRAIN.n_f, msd_data.TRAIN.n_f, len(scenario_list)))
+        B_all = np.zeros((msd_data.TRAIN.n_f, msd_data.TRAIN.n_u, len(scenario_list)))
+        for i_scenario, scenario_tuple in enumerate(scenario_list):
+            system_layer_interp = scenario_tuple[1]
+            msd_data_id_interp_scenario = scenario_tuple[0]
+            mu_orig = scenario_tuple[4]
+            J, R, B, Q = system_layer_interp.get_system_matrices(
+                None, msd_data_id_interp_scenario.TRAIN.n_t
+            )
+            Q_all[:, :, i_scenario] = Q
+            J_all[:, :, i_scenario] = J
+            R_all[:, :, i_scenario] = R
+            B_all[:, :, i_scenario] = B
+            A_all[:, :, i_scenario] = (J - R) @ Q
+            # list_matrices.append(A)
+            parameter_array[i_scenario, :] = mu_orig
+        save_matrices = True
+        if save_matrices:
+            np.savez(
+                os.path.join(result_dir, "JRBQ_matrix_interpolation.npz"),
+                J=J_all,
+                R=R_all,
+                Q=Q_all,
+                B=B_all,
+                A=A_all,
+                parameter_array=parameter_array,
+            )
+
+    # test with sample data:
+    test_with_sample_data = False
+    if test_with_sample_data:
+        weighting_array = get_weighting_function_values(
+            parameter_array, parameter_array, ansatz="linear"
         )
-        scenario_list.append((msd_data_id, system_layer, phin, msd_data))
+        matrices_eval = evaluate_matrices(A_all, weighting_array)
+        print(np.allclose(A_all, matrices_eval))  # should be True
+
+    _, idx = np.unique(msd_data_orig.TEST.Mu, axis=0, return_index=True)
+    parameter_test = msd_data_orig.TEST.Mu[np.sort(idx), :]
+
+    plot_convex_hull = False
+    if plot_convex_hull:
+        hull = ConvexHull(parameter_array)
+        fig = plt.figure(figsize=(12, 12))
+        ax = fig.add_subplot(projection="3d")
+        ax.scatter(
+            parameter_array[:, 0], parameter_array[:, 1], parameter_array[:, 2], "b"
+        )
+        ax.scatter(
+            parameter_test[:, 0], parameter_test[:, 1], parameter_test[:, 2], "r"
+        )
+        for s in hull.simplices:
+            s = np.append(s, s[0])  # Here we cycle back to the first coordinate
+            ax.plot(
+                parameter_array[s, 0],
+                parameter_array[s, 1],
+                parameter_array[s, 2],
+                "r-",
+            )
+        plt.show()
+
+    weighting_array = get_weighting_function_values(
+        parameter_array, parameter_test, ansatz="linear"
+    )
+    # TODO: compare PH vs LTI
+
+    A_interp = evaluate_matrices(A_all, weighting_array)
+    B_interp = evaluate_matrices(B_all, weighting_array)
+
+    latent_shape = (
+        msd_data_orig.TEST.n_sim,
+        msd_data_orig.TEST.n_t,
+        system_layer_interp.r,
+    )
+    Z_ph = np.zeros(latent_shape)
+    Z_dt_ph = np.zeros(latent_shape)
+    for i_test in range(A_interp.shape[2]):
+        sim_indices = (i_test) * 3 + np.array([0, 1, 2])
+        lti_system = LTISystem(A=A_interp[:, :, i_test], B=B_interp[:, :, i_test])
+        for i_sim in sim_indices:
+            u = msd_data_orig.TEST.U[i_sim]
+            msd_data_orig.TEST.get_initial_conditions()
+            x_init = np.expand_dims(msd_data_orig.TEST.x_init[i_sim, :], axis=0).T
+            Z_ph[i_sim], Z_dt_ph[i_sim] = lti_system.solve_dt(
+                msd_data_orig.TEST.t,
+                x_init,
+                u,
+            )
+    z_ph, dz_dt_ph = reshape_states_to_features(Z_ph, Z_dt_ph)
+    x_ph, dx_dt_ph = z_ph, dz_dt_ph
+    X_ph, X_dt_ph = reshape_features_to_states(
+        x_ph,
+        msd_data_orig.TEST.n_sim,
+        msd_data_orig.TEST.n_t,
+        x_dt=dx_dt_ph,
+        n_n=msd_data_orig.TEST.n_n,
+        n_dn=msd_data_orig.TEST.n_dn,
+    )
+    z = msd_data_orig.TEST.x
+    Z = reshape_features_to_states(
+        z, msd_data_orig.TEST.n_sim, msd_data_orig.TEST.n_t, n_f=system_layer_interp.r
+    )
+    z_dt = msd_data_orig.TEST.dx_dt
+    Z_dt = reshape_features_to_states(
+        z_dt,
+        msd_data_orig.TEST.n_sim,
+        msd_data_orig.TEST.n_t,
+        n_f=system_layer_interp.r,
+    )
+    msd_data_id_interp = PHIdentifiedDataset()
+    msd_data_id_interp.TEST = PHIdentifiedData(
+        t=msd_data_orig.TEST.t,
+        X=X_ph,
+        X_dt=X_dt_ph,
+        Z=Z,
+        Z_dt=Z_dt,
+        Z_ph=Z_ph,
+        Z_dt_ph=Z_dt_ph,
+        Mu=msd_data_orig.TEST.Mu,
+    )
+    msd_data_id_interp.TEST.states_to_features()
+
+    use_train_data = False
+    idx_gen = "rand"
+
+    result_dir_interp = f"{result_dir}_interp"
+    if not os.path.isdir(result_dir_interp):
+        os.mkdir(result_dir_interp)
+    msd_data_orig.TEST.calculate_errors(msd_data_id_interp.TEST)
+    aphin_vis.plot_time_trajectories_all(
+        msd_data_orig, msd_data_id_interp, use_train_data, idx_gen, result_dir_interp
+    )
+
+    # diff_system_matrix = system_matrix[:, :, 0] - np.squeeze(list_matrices[0], axis=0)
+    # print(diff_system_matrix)
 
     _, _, _, _, mu_test = msd_data.test_data
     n_t_test = msd_data.n_t_test
-    print_matrices(system_layer, mu=mu_test, n_t=n_t_test, data=msd_data)
-    save_evaluation_times(msd_data_id, result_dir)
+    print_matrices(system_layer_interp, mu=mu_test, n_t=n_t_test, data=msd_data)
+    save_evaluation_times(msd_data_id_interp_scenario, result_dir)
 
-    msd_data.calculate_errors(msd_data_id, domain_split_vals=[1, 1])
-    use_train_data = False
+    msd_data.calculate_errors(msd_data_id_interp_scenario, domain_split_vals=[1, 1])
+
     aphin_vis.plot_errors(
         msd_data,
         use_train_data,
         save_name=os.path.join(result_dir, "rms_error"),
     )
 
-    msd_data.calculate_errors(msd_data_id, save_to_txt=True, result_dir=result_dir)
+    msd_data.calculate_errors(
+        msd_data_id_interp_scenario, save_to_txt=True, result_dir=result_dir
+    )
 
     # plot state values
-    idx_gen = "rand"
     aphin_vis.plot_time_trajectories_all(
-        msd_data, msd_data_id, use_train_data, idx_gen, result_dir
+        msd_data, msd_data_id_interp_scenario, use_train_data, idx_gen, result_dir
     )
 
     # plot chessboard visualisation
@@ -227,3 +459,5 @@ if __name__ == "__main__":
     working_dir = os.path.dirname(__file__)
     config_file_path = os.path.join(working_dir, "config_msd_matrix_interpolation.yml")
     main(config_file_path)
+
+# %%
--- examples/mass_spring_damper/state_space_ph/matrix_interpolation.py
+++ examples/mass_spring_damper/state_space_ph/matrix_interpolation.py
@@ -1,7 +1,266 @@
 import numpy as np
+import scipy
+import scipy.integrate
+import scipy.interpolate
+import scipy.linalg
 
 
-def matrix_interpolation(
-    list_matrices: list, parameter_array: np.ndarray, ansatz: str = "linear"
+def check_list_matrices(list_matrices):
+    if list_matrices[0].ndim == 3:
+        if list_matrices[0].shape[0] != 1:
+            raise ValueError(f"The matrix should not contain different simulations.")
+        else:
+            list_matrices = [
+                np.squeeze(list_matrices[i], axis=0) for i in range(len(list_matrices))
+            ]
+    return list_matrices
+
+
+def prepare_linear_system_equations(
+    matrices: list | np.ndarray,
+    parameter_array: np.ndarray,
+    ansatz: str = "linear",
+    evaluation=False,
 ):
-    pass
+    """
+    parameter_array (n_systems x n_param)
+    """
+    if isinstance(matrices, np.ndarray):
+        assert matrices.shape[2] == parameter_array.shape[0]
+        n0 = matrices.shape[0]
+        n1 = matrices.shape[1]
+    elif isinstance(matrices, list):
+        assert len(matrices) == parameter_array.shape[0]
+        matrices = check_list_matrices(matrices)
+        n0 = matrices[0].shape[0]
+        n1 = matrices[0].shape[1]
+    else:
+        raise ValueError(f"Unknown type {type(matrices)} of the matrices.")
+
+    n_system = parameter_array.shape[0]
+    n_param = parameter_array.shape[1]
+
+    if evaluation:
+        n_system_eval = 1
+    else:
+        n_system_eval = n_system
+
+    if isinstance(matrices, np.ndarray):
+        system_matrices = matrices
+    elif isinstance(matrices, list):
+        # create matrices of size n0 x n1 x n_system
+        system_matrices = np.zeros((n0, n1, n_system))
+        for i_system in range(len(matrices)):
+            system_matrices[:, :, i_system] = matrices[i_system]
+
+    if ansatz == "linear":
+        n_coeff = (n_param + 1) * n_system
+        matrix_parameter_array = np.zeros((n0 * n1 * n_system_eval, n_coeff))
+        matrix_entries_1d = np.zeros((n0 * n1 * n_system_eval, 1))
+        # set up parameter entries
+        parameter_entry_format = np.ones((n_coeff, n_system))
+        for i_system in range(n_system):
+            param_plus_one = np.ones((n_system, n_param + 1))
+            param_plus_one[:, 1:] = np.repeat(
+                np.expand_dims(parameter_array[i_system, :], axis=0), n_system, axis=0
+            )
+            parameter_entry_format[:, i_system] = param_plus_one.flatten()
+    else:
+        raise NotImplementedError(
+            f"The ansatz {ansatz} has currently not been implemented."
+        )
+
+    # loop over columns and rows
+    i_entry = 0
+    for i_row in range(n0):
+        for j_column in range(n1):
+            for k_system in range(n_system_eval):
+                matrix_entries_ij = system_matrices[i_row, j_column, :]
+                matrix_entries_ij_repeated = np.repeat(
+                    np.expand_dims(matrix_entries_ij, axis=1), n_param + 1, axis=1
+                ).flatten()
+                matrix_parameter_array[i_entry, :] = (
+                    matrix_entries_ij_repeated * parameter_entry_format[:, k_system]
+                )
+                matrix_entries_1d[i_entry] = system_matrices[i_row, j_column, k_system]
+                i_entry += 1
+
+    if evaluation:
+        return matrix_parameter_array
+    else:
+        return matrix_parameter_array, matrix_entries_1d
+
+
+def get_coeff_values(
+    matrices: list, parameter_array: np.ndarray, ansatz: str = "linear"
+):
+    matrix_parameter_array, matrix_entries_1d = prepare_linear_system_equations(
+        matrices=matrices, parameter_array=parameter_array, ansatz=ansatz
+    )
+
+    # solve least square problem
+    coeff_values, residuals, rank, sing_val = np.linalg.lstsq(
+        matrix_parameter_array, matrix_entries_1d
+    )
+
+    return coeff_values
+
+
+def evaluate_interpolation(
+    coeff_values, training_matrices_list, parameter_array_eval, ansatz
+):
+
+    if parameter_array_eval.ndim == 1:
+        parameter_array_eval = np.expand_dims(parameter_array_eval, axis=0)
+
+    training_matrices_list = check_list_matrices(training_matrices_list)
+
+    n_system_eval = parameter_array_eval.shape[0]
+    n_system_training = len(training_matrices_list)
+    n0 = training_matrices_list[0].shape[0]
+    n1 = training_matrices_list[0].shape[1]
+
+    system_matrix = np.zeros((n0, n1, n_system_eval))
+    for i_system_eval in range(n_system_eval):
+        parameter_array_eval_i_system = np.repeat(
+            np.expand_dims(parameter_array_eval[i_system_eval, :], axis=0),
+            n_system_training,
+            axis=0,
+        )
+        matrix_parameter_array = prepare_linear_system_equations(
+            matrices=training_matrices_list,
+            parameter_array=parameter_array_eval_i_system,
+            ansatz=ansatz,
+            evaluation=True,
+        )
+
+        matrix_entries_i_system = matrix_parameter_array @ coeff_values
+        system_matrix[:, :, i_system_eval] = np.reshape(
+            matrix_entries_i_system, (n0, n1)
+        )
+
+    return system_matrix
+
+
+def get_weighting_function_values(
+    parameter_samples_train: np.ndarray,
+    parameter_samples_eval: np.ndarray,
+    ansatz: str = "linear",
+):
+    """
+    parameter_samples_train: n_system x n_param array
+    parameter_samples_eval: n_eval_points x n_param array
+    Returns:
+    array of (n_system,n_eval_points) weighting values
+    """
+    assert parameter_samples_train.shape[1] == parameter_samples_eval.shape[1]
+
+    n_system = parameter_samples_train.shape[0]
+    n_eval_points = parameter_samples_eval.shape[0]
+
+    weighting_array = np.zeros((n_system, n_eval_points))
+    for i_system in range(n_system):
+        values = np.zeros(n_system)
+        values[i_system] = 1
+        weighting_array[i_system, :] = scipy.interpolate.griddata(
+            points=parameter_samples_train,
+            values=values,
+            xi=parameter_samples_eval,
+            method=ansatz,
+        )
+
+    print(
+        f"Max sum difference: {np.max(np.abs(np.sum(weighting_array, axis=0)- np.ones(n_eval_points)))}"
+    )
+    # assert np.allclose(np.sum(weighting_array, axis=0), np.ones(n_eval_points))
+
+    return weighting_array
+
+
+def evaluate_matrices(matrices_training: np.ndarray, weighting_array: np.ndarray):
+    """
+    matrices_training (n0,n1,n_system): matrices gained from training parameter samples parameter_samples_train
+    weighting_array (n_system,n_eval_points): usually calculated with get_weighting_function_values
+    Returns:
+    requested_matrices of size (n0,n1,n_eval_points)
+    """
+    assert matrices_training.shape[2] == weighting_array.shape[0]
+
+    n0 = matrices_training.shape[0]
+    n1 = matrices_training.shape[1]
+    n_system = matrices_training.shape[2]
+    n_eval_points = weighting_array.shape[1]
+
+    requested_matrices = np.zeros((n0, n1, n_eval_points))
+    for i_eval_point in range(n_eval_points):
+        for i_system in range(n_system):
+            requested_matrices[:, :, i_eval_point] = (
+                requested_matrices[:, :, i_eval_point]
+                + weighting_array[i_system, i_eval_point]
+                * matrices_training[:, :, i_system]
+            )
+    return requested_matrices
+
+
+def in_hull(points, x):
+    # https://stackoverflow.com/questions/16750618/whats-an-efficient-way-to-find-if-a-point-lies-in-the-convex-hull-of-a-point-cl
+    n_points = len(points)
+    n_dim = len(x)
+    c = np.zeros(n_points)
+    A = np.r_[points.T, np.ones((1, n_points))]
+    b = np.r_[x, np.ones(1)]
+    lp = scipy.optimize.linprog(c, A_eq=A, b_eq=b)
+    return lp.success
+
+    # # matrices_conc = np.squeeze(np.concatenate((list_matrices), axis=1), axis=0)
+    # # matrix_entries_1d = matrices_conc.flatten()
+
+    # # matrix_entries_diag = np.diag(matrix_entries_1d)
+
+    # # size_matrix = list_matrices[0].shape
+    # # linear_equations = size_matrix[0] * size_matrix[1] * n_system
+
+    # if ansatz == "linear":
+    #     # initialize arrays
+    #     n_coeff = (n_param + 1) * n_system
+    #     matrix_parameter_array = np.zeros(
+    #         (size_matrix[0] * size_matrix[1] * n_system, n_coeff)
+    #     )
+    #     for i_entries in range(matrix_entries_1d.shape[0]):
+    #         matrix_entry = np.repeat(
+    #             np.expand_dims(matrix_entries_1d[i_entries], axis=1),
+    #             n_param + 1,
+    #             axis=1,
+    #         )
+    #     # parameter values
+
+    #     param_matrix = []
+    #     for i_system in range(n_system):
+    #         params_i = np.ones((size_matrix[0] * size_matrix[1], n_param + 1))
+    #         params_i[:, 1:] = np.repeat(
+    #             np.expand_dims(parameter_array[i_system, :], axis=0),
+    #             size_matrix[0] * size_matrix[1],
+    #             axis=0,
+    #         )
+    #         param_matrix.append(params_i)
+    #     param_matrix = scipy.linalg.block_diag(*param_matrix)
+    #     assert linear_equations == param_matrix[0]
+
+    # # solve linear system of equations
+    # # system of the form
+    # # matrix_entries_diag@param_matrix @ coeff_values = matrix_entries_1d
+    # # where matrix_entries_diag contains all matrix entries in diagonal structure, i.e. diag matrix of size n*n*n_system
+    # # param_matrix uses a block-diagonal structure with constant term and parameter terms, i.e.
+    # # [1 p1s1 p2s1 0 0 0|
+    # # 1 p1s1 p2s1  0 0 0| repeated n*n times
+    # # 1 p1s1 p2s1  0 0 0|
+    # # 0 0   0 1 p1s2 p2s2 |
+    # # 0 0   0 1 p1s2 p2s2 | repeated n*n times
+    # # 0 0   0 1 p1s2 p2s2 |
+    # # ]
+    # # matrix_entries_1d contains all matrix entries in vector form
+    # coeff_values, residuals, rank, sing_val = np.linalg.lstsq(
+    #     matrix_entries_diag @ param_matrix, matrix_entries_1d
+    # )
+
+    # return coeff_values
